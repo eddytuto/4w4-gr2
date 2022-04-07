dc708bd769b5e463bbe1e72ad13ae933b470f983
Eddy Martin <eddypmartin@gmail.com>
2022-04-07 17:21:06 -0400

    s10-c2 category.php lien asc desc acf

--- front-page.php
+++ front-page.php
@@ -8,11 +8,18 @@
     <div class="animation__bloc">4</div>
     <div class="animation__bloc">5</div>
 </section>
-
-    <?php wp_nav_menu(array("menu"=>"menu_accueil",
+    <h1>Les études en TIM</h1>
+    
+    <?php 
+    /* --------------------------------------------- menu accueil */
+    wp_nav_menu(array("menu"=>"accueil",
+                            "container"=>"nav")); ?>
+    
+   <?php 
+   /* --------------------------------------------- menu evenement */
+   wp_nav_menu(array("menu"=>"evenement",
                             "container"=>"nav")); ?>
 
-    <h1>---- Front-page.php ------</h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>   
