3181deb44ee591164da24ccc2a2f5e20664283b2
Eddy Martin <eddypmartin@gmail.com>
2022-04-14 16:38:50 -0400

    s11-c2 Développement de template-evenement.php ajout des champs personnalisés

--- gabarit/content-cours.php
+++ gabarit/content-cours.php
@@ -11,8 +11,9 @@ $categories = get_the_category();
     $titreFiltreCours = substr($titre, 3, -6);
     // $nbHeures = substr($titre, -6);
     $nbHeures = get_field( "nombre_dheures" );
+    $departement = get_field( "departement" );
     $sigleCours = substr($titre, 0, 4);
-    $descCours = get_the_excerpt();
+    $descCours = wp_trim_words(get_the_content(),15,"<a href='#'> La suite </a>");
     ?>
     <?php the_post_thumbnail("thumbnail") ?>
     <h3 class="cours__titre">
@@ -23,4 +24,5 @@ $categories = get_the_category();
     <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
     <p class="cours__sigle"><?= $sigleCours; ?> </p>
     <p class="cours__desc"> <?= $descCours; ?></p>
+    <p class="cours__departement"><?= $departement; ?></p>
 </article>
