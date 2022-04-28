9bcad4afdcc59c1113891ca4bac6119566e84788
Eddy Martin <eddypmartin@gmail.com>
2022-04-28 15:19:04 -0400

    s13-c1 finalisation du Customizer avec les propriété de background-color

--- header.php
+++ header.php
@@ -9,11 +9,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <?php wp_head() ?>
+    <!-- style>
+        .home::after{
+            background-color: ;
+        }
+
+        .site__main {
+            background-color: ;
+        }
+    </style -->
+
     <?php show_admin_bar(true); ?>
 </head>
-<body  <?php body_class("site"); ?>>
+<body  <?php body_class("site"); ?> style="background-color:<?= get_theme_mod("background_body"); ?>;" >
 <header class="site__header">
     <section class="site__header__titre">
         <?= get_custom_logo(); ?>
